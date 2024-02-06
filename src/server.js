4d2f0fbe3b7eaa4b28cea4e3cb2814020d16ace8
Tofeeq <tofeeq09@gmail.com>
2024-02-06 10:30:14 +0000

    deleted: rod folder and files

--- src/server.js
+++ src/server.js
@@ -6,15 +6,6 @@ const app = express();
 //   res.send("healthy");
 // });
 
-app.post("/RideOrDie/contact.html", (req, res) => {
-  // Handle the POST request here
-  // For example, you might parse the request body and save the data
-
-  // Then, send a response back to the client
-
-  res.send("Thank you for your submission!");
-});
-
 app.use("/RideOrDie", express.static("rod"));
 
 app.listen(5001, () => {
